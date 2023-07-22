e47d805c6c075f87b8596c7236a7e9aad9963432
poisklmpoikslms901i234 <98320987+poisklmpoikslms901i234@users.noreply.github.com>
2023-07-22 18:59:04 +0700

    Update .lua

--- .lua
+++ .lua
@@ -2803,7 +2803,7 @@ function library:Load(options)
     end
 
     local cursor = utility.create("Triangle", {
-        Thickness = 2.5,
+        Thickness = 15,
         Color = Color3.fromRGB(255, 255, 255),
         ZIndex = 1000
     })
